a5d6226049bb2e3c3d6c2d1f2eedae8002bec934
Stian Didriksen <stipsan@gmail.com>
2016-03-09 20:52:33 +0100

    Add React through webpack.ProvidePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,16 @@ plugins = plugins.concat(new ExtractTextPlugin("[name].css?[hash]", {
   disable: process.env.NODE_ENV !== 'production'
 }));
 
+// JSX syntax is transpiled to React.createElement calls with babel, which is why devs
+// often do `import React from 'react'` even if `React` itself isn't used in the source.
+// it's also why variations like `import React, {Component, PropTypes} from 'react'` is seen in the wild.
+// However, when modules can be inlined using `transform-react-inline-elements`
+// importing `React` isn't necessary as babel replaces the JSX with whatever the React.createElement
+// returns on the call-site, reducing bloat and laying the ground work for tree shaking in webpack v2
+plugins = plugins.concat(new webpack.ProvidePlugin({
+  React: 'react',
+}));
+
 var AssetsPlugin = require('assets-webpack-plugin');
 plugins = plugins.concat(new AssetsPlugin({filename: 'assets.json', path: path.join(__dirname, 'server')}));
 
