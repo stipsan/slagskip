c4a5667954c4ce1422ec67992c8c75d78276ed81
Stian Didriksen <stipsan@gmail.com>
2016-03-09 22:17:29 +0100

    styled loginform

--- client/components/pages/Login.js
+++ client/components/pages/Login.js
@@ -4,7 +4,7 @@ const buttonLabel = 'Enter';
 export default function Login() {
   return <section className="section section--login">
     <form>
-      <input placeholder={placeholderLabel} />
+      <input placeholder={placeholderLabel} minLength={4} required={true} />
       <button type="submit">{buttonLabel}</button>
     </form>
   </section>;
