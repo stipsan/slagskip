ded563404f675132ee7910b3e7c723cd9c8d4669
Stian Didriksen <stipsan@gmail.com>
2016-03-09 19:45:41 +0100

    about to render html dynamically

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use(express.static('public'));
 app.use(fallback('index.html', {root: path.join(__dirname, 'public')}));
 
 app.listen(port, function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', port);
 });
---
+++ server/html.js
@@ -0,0 +1,14 @@
+//@TODO implement using dataloader
+
+module.exports = function(assets){
+  return `<!doctype html>
+<html>
+  <head>
+    <title>Slagskip</title>
+    <script src="/client.js" async></script>
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>`;
+};
