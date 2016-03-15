07fd2bfcf3100dfff55ec5743f373d84b3ed00d6
Stian Didriksen <stipsan@gmail.com>
2016-03-15 02:51:14 +0100

    working on migrations

--- server/migrate/index.js
+++ server/migrate/index.js
@@ -1,7 +1,22 @@
+// @TODO could prove to be a useful reusable module on npm
+
 var Redis = require('ioredis');
 var redis = new Redis(process.env.REDIS_URL);
 
 // it is a bit primitive for now, but it is a good start
 redis.setnx('next_user_id', 0);
+
+/* later, let's do something like this for real migrations
+redis.get('last_migration', id => {
+  let batch = [];
+  switch (id) {
+    default:
+      
+  }
+    batch.push()
+    
+})
+
+*/
 
 redis.quit();
