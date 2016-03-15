ab247762fb58260ebd6ee334074e75ee45a0a5fd
Stian Didriksen <stipsan@gmail.com>
2016-03-15 01:39:25 +0100

    working on a migration

--- server/migrate.js
+++ server/migrate.js
@@ -1,4 +1,13 @@
 var Redis = require('ioredis');
 var redis = new Redis(process.env.REDIS_URL);
 
+console.log('migration started');
+
+console.log('it is a bit primitive for now, but it is a good start :-)');
 redis.set('next_user_id', 1);
+
+
+
+setTimeout(() => console.log('About to complete'), 1000);
+
+setTimeout(() => process.exit(), 2000);
