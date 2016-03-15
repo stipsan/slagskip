67966ee6cad145949f414d2a7c59e1d471a3e6b7
Stian Didriksen <stipsan@gmail.com>
2016-03-15 01:06:37 +0100

    restructuring code a bit

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ var SocketCluster = require('socketcluster').SocketCluster;
 var socketCluster = new SocketCluster({
   workers: Number(process.env.WEB_CONCURRENCY) || 1,
   port: process.env.PORT || 5000,
-  workerController: __dirname + '/web.js',
+  workerController: __dirname + '/worker.js',
 });
