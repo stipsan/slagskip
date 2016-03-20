9016dae0a9a3983f70e80c111e059e180a27775f
Stian Didriksen <stipsan@gmail.com>
2016-03-20 18:35:15 +0100

    Fix test runners that fail on scss import paths

--- test/css-modules-compiler.js
+++ test/css-modules-compiler.js
@@ -1,11 +1,13 @@
 var hook = require('css-modules-require-hook');
 var sass = require('node-sass');
+var path = require('path');
 
 hook({
   extensions: ['.scss'],
-  preprocessCss: function (css) {
+  preprocessCss: function (css, filepath) {
     var result =  sass.renderSync({
-      data: css
+      data: css,
+      includePaths: [ path.resolve(filepath, '..') ]
     });
 
     return result.css;
