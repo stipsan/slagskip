76e5f953dae4187c7283d184b6e3c4465fd991d6
Stian Didriksen <stipsan@gmail.com>
2016-03-30 19:58:38 +0200

    adding avatars

--- client/components/NewGame/Friend.js
+++ client/components/NewGame/Friend.js
@@ -1,6 +1,30 @@
 import { Component } from 'react'
 import { Link } from 'react-router'
+import Avatar from 'react-user-avatar'
 import style from './style.scss'
+
+const defaultColors = [
+  '#1abc9c',
+  '#2ecc71',
+  '#3498db',
+  '#9b59b6',
+  '#34495e',
+  '#16a085',
+  '#27ae60',
+  '#2980b9',
+  '#8e44ad',
+  '#2c3e50',
+  '#f1c40f',
+  '#e67e22',
+  '#e74c3c',
+  '#ecf0f1',
+  '#95a5a6',
+  '#f39c12',
+  '#d35400',
+  '#c0392b',
+  '#bdc3c7',
+  '#7f8c8d'
+]
 
 class Friend extends Component {
   render() {
@@ -8,8 +32,11 @@ class Friend extends Component {
     const username = friend.get('username')
     const id = friend.get('id')
     return <Link to={`setup/${id}`} className={style.friend}>
-      
-      {friend.get('username')}
+      <span className={style.avatar}>
+        <span><Avatar colors={defaultColors} size="39" name={username} /></span>
+      </span>
+      <span className={style.username}>{friend.get('username')}</span>
+      <span className={style.startGame}>❯</span>
     </Link>
   }
 }
