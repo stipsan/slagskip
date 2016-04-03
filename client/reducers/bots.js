9ab7da4daba9d97f314d348cce4809e80f6550a8
Stian Didriksen <stipsan@gmail.com>
2016-04-03 23:36:05 +0200

    Introducing HAL-9000

--- client/reducers/bots.js
+++ client/reducers/bots.js
@@ -5,8 +5,8 @@ const initialState = fromJS([
   { id: "-2", username: 'R2D2', online: '1', avatar: '/bot/r2d2.png', description: `A bit harder to beat` },
   { id: "-3", username: 'K-9', online: '1', avatar: '/bot/k9.png', description: `Sniffs out your hiding spots` },
   { id: "-4", username: 'Marvin', online: '1', avatar: '/bot/marvin.png', description: `Gets depressed if he beats you` },
-  //{ id: "-5", username: 'HAL 9000', online: '1', avatar: '/bot/hal.png', status: `Makes very few mistakes` },
-  //{ id: "-6", username: 'GLaDOS', online: '1', avatar: '/bot/glados.png', status: `She'll give you cake if you win` },
+  { id: "-5", username: 'HAL 9000', online: '1', avatar: '/bot/hal.png', description: `Makes very few mistakes` },
+  //{ id: "-6", username: 'GLaDOS', online: '1', avatar: '/bot/glados.png', description: `She'll give you cake if you win` },
 ])
 
 export const bots = (state = initialState) => {
