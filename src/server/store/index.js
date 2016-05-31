9de7eb968927dd302d1aef7f634859b4fb4299fd
Stian Didriksen <stipsan@gmail.com>
2016-05-31 10:23:53 +0200

    implementing redux saga on the server

--- src/server/store/index.js
+++ src/server/store/index.js
@@ -1,19 +1,23 @@
 import * as reducers from '../reducers'
 
 import createSagaMiddleware from 'redux-saga'
+import devTools from 'remote-redux-devtools'
 import thunk from 'redux-thunk'
 import { Map as ImmutableMap } from 'immutable'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { combineReducers } from 'redux-immutable'
 
 import sagas from '../sagas'
 
 export default (socket, database, redis) => {
   const sagaMiddleware = createSagaMiddleware()
+  const middleware = applyMiddleware(thunk, sagaMiddleware)
+  const enhancer = 'production' !== process.env.NODE_ENV ?
+    compose(middleware, devTools()) : middleware
   const store = createStore(
     combineReducers(reducers),
     ImmutableMap(),
-    applyMiddleware(thunk, sagaMiddleware)
+    enhancer
   )
 
   sagaMiddleware.run(sagas, socket, database, redis)
