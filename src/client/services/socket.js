832cfd609742c5f81c03e8c37e833173d6451317
Stian Didriksen <stipsan@gmail.com>
2016-06-02 22:51:35 +0200

    limit pingback to no more than 10s

--- src/client/services/socket.js
+++ src/client/services/socket.js
@@ -6,4 +6,5 @@ export const socket = socketCluster && socketCluster.connect({
   hostname: process.env.SOCKET_HOSTNAME || location.hostname,
   autoReconnect: true,
   autoReconnectOptions: process.env.AUTO_RECONNECT_OPTIONS,
+  ackTimeout: 10000, // server should never take this long to ping back
 })
