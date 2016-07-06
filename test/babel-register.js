0474c5741c87f4925b10de13f17e26859d31c9df
Stian Didriksen <stipsan@gmail.com>
2016-07-06 15:35:39 +0200

    istanbul babel plugin added

--- test/babel-register.js
+++ test/babel-register.js
@@ -2,5 +2,8 @@ require('babel-register')({
   presets: ['react', 'es2015', 'stage-0'].map(preset =>
     require.resolve(`babel-preset-${preset}`)
   ),
+  plugins: ['istanbul'].map(plugin =>
+    require.resolve(`babel-plugin-${plugin}`)
+  ),
   babelrc: false,
 })
