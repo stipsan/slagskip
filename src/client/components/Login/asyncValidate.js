fa6fad7bb5d94096582f35b90f40b73de871d258
Stian Didriksen <stipsan@gmail.com>
2016-08-22 16:38:30 +0200

    asyncValidate test

--- src/client/components/Login/asyncValidate.js
+++ src/client/components/Login/asyncValidate.js
@@ -3,9 +3,11 @@ import {
 } from '../../constants/ActionTypes'
 
 const asyncValidate = (values, dispatch) => new Promise((resolve, reject) =>
-  dispatch({ type: CHECK_EMAIL_EXISTS_ASYNC, payload: {
-    email: values.get('email'), resolve, reject
-  } })
+  dispatch({
+    type: CHECK_EMAIL_EXISTS_ASYNC, payload: {
+      email: values.get('email'), resolve, reject
+    }
+  })
 )
 
 export default asyncValidate
