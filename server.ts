5a86c799e37562d5b480fc1a16b3cede88658caa
Stian Didriksen <stipsan@gmail.com>
2017-10-15 20:56:46 +0200

    next + micro = <3

--- server.ts
+++ server.ts
@@ -1,7 +1,32 @@
-import server from '@epic/server'
+import micro from 'micro'
+import { parse } from 'url'
+import * as next from 'next'
 
-// Pass down env variables to reduce hits to process.env to a minimum
-// (accessing process.env is very slow, the more you access it the bigger the penalty)
-server({
-  dev: process.env.NODE_ENV !== 'production',
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const server = micro(async (req, res) => {
+  const parsedUrl = parse(req.url, true)
+  const { pathname, query } = parsedUrl
+
+  console.log(parsedUrl, parsedUrl.pathname)
+
+  if (pathname === '/graphql') {
+    return 'graphql here'
+  }
+
+  if (pathname === '/graphiql') {
+    return 'Graphiql IDE here'
+  }
+
+  return handle(req, res, parsedUrl)
+})
+
+app.prepare().then(() => {
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
