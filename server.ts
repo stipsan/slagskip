e3a8c31cd8d5a541c66553f4ad4b1bb8c61f538f
Stian Didriksen <stipsan@gmail.com>
2017-10-15 04:50:05 +0200

    adding typings for next

--- server.ts
+++ server.ts
@@ -3,5 +3,5 @@ import server from '@epic/server'
 // Pass down env variables to reduce hits to process.env to a minimum
 // (accessing process.env is very slow, the more you access it the bigger the penalty)
 server({
-  dev: process.env.NODE_ENV !== 'production'
+  dev: process.env.NODE_ENV !== 'production',
 })
